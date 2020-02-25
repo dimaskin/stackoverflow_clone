72d8da54bc10c1a82c6a9ddad3efd48bb637711f
dd <dmitriy.a.drachev@gmail.com>
2020-02-25 18:45:23 +0300

    add some fix to tests. final

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,19 +10,22 @@ RSpec.describe User, type: :model do
   let!(:answer) { create :answer, question: question, author: user }
 
   it 'is user an author of the question' do
-    expect(user.author?(question)).to be_truthy
+    #expect(user.author?(question)).to be_truthy
+    expect(user).to be_author(question)
   end
 
   it 'is user an author of the answer' do
-    expect(user.author?(answer)).to be_truthy
+    #expect(user.author?(answer)).to be_truthy
+    expect(user).to be_author(answer)
   end
 
   it 'user is not an author of the question' do
-    expect(non_author.author?(question)).to be_falsey
+    #expect(non_author.author?(question)).to be_falsey
+    expect(non_author).not_to be_author(question)
   end
 
   it 'user is not an author of the answer' do
-    expect(non_author.author?(answer)).to be_falsey
+    expect(non_author).not_to be_author(answer)
   end
 
 end
